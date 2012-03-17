886f2daad388d70cc88780688f4cc78cafaafb0f
Justin Domingue <justin.domingue@hotmail.com>
2012-03-17 12:50:06 -0400

    precompiled assets

--- config/application.rb
+++ config/application.rb
@@ -46,6 +46,6 @@ module Mobile
     config.assets.version = '1.0'
     
     config.assets.initialize_on_precompile = false
-    config.assets.precompile += ['matieres.png']
+    config.assets.precompile += ['icon/matieres.png']
   end
 end
