b2575e6cd1c72126aeb78896836b998477fc2b3c
Justin Domingue <justin.domingue@hotmail.com>
2012-04-01 11:31:09 -0400

    improved css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,6 @@ Mobile::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.assets.precompile += [ "*.css" ]
 end
