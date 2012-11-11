72712c1fd7ce8ab492256a14b0e977849847de72
BR41N1 <justin.domingue@hotmail.com>
2012-11-11 15:00:44 -0500

    added Search to ActiveAdmin

--- app/admin/dashboards.rb
+++ app/admin/dashboards.rb
@@ -2,14 +2,25 @@
 # encoding: utf-8
 
 ActiveAdmin::Dashboards.build do
-  section "Produits récents" do
+  
+  section "Recherches récentes", priority:1 do
+    table_for Search.order("created_at desc").limit(5) do
+      column "Nom", :name do |product|
+        link_to product.name, admin_matiere_path(product)
+      end
+    end
+    strong { link_to "Voir toutes les Recherches", admin_searches_path }
+  end
+
+  section "Produits récents", priority:2 do
     table_for Product.order("created_at desc").limit(5) do
       column "Nom", :name do |product|
         link_to product.name, admin_matiere_path(product)
       end
     end
-    strong { link_to "Voir tous les Products", admin_matieres_path }
+    strong { link_to "Voir tous les Produits", admin_matieres_path }
   end
+
   section "Places récentes" do
     table_for Place.order("created_at desc").limit(5) do
       column "Nom", :name do |place|
