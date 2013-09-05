e430397e5aa24b23a4273884e08c98990f573c91
Justin Domingue <justin.domingue@hotmail.com>
2013-09-04 21:09:26 -0400

    Added location:nil to School

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -7,12 +7,12 @@ class SchoolsController < ApplicationController
     if params[:school]
       @school = School.find_by_id params[:school]
       if @school
-        respond_with @school
+        respond_with @school, :location => nil
       else
         respond_with School.first, meta: { message:'Établissement introuvable. Base de données par défaut chargée.', error:'No school responds to the given ID (' + params[:school] + ').' }
       end
     else
-      respond_with error:'No school provided', status:400
+      respond_with message:"Aucun établissement spécifié.", status:400
     end
   end
 end
