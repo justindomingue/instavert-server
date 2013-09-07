35f6c42654a31bf6efb5694b7c0b1ec3ae73f485
Justin Domingue <justin.domingue@hotmail.com>
2013-09-07 18:53:13 -0400

    Added support for category in the serializer

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,7 +1,12 @@
 class ProductSerializer < ActiveModel::Serializer
   embed :ids
-  attributes :id, :name, :tags, :views
+  attributes :id, :name, :tags, :category
   
   has_many :places
   
+  def category
+    unless object.category.nil?
+      object.category.gsub(/\s+/, "_").downcase
+    end
+  end
 end
