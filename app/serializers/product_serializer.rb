d7965b4fe1ebebf383a457d6e23a02b55140bf24
Justin Domingue <justin.domingue@hotmail.com>
2013-09-09 21:33:07 -0400

    Product now serves thumbs and colors (json)

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,13 +1,41 @@
+#!/bin/env ruby
+# encoding: utf-8
+
 class ProductSerializer < ActiveModel::Serializer
   embed :ids
-  attributes :id, :name, :tags, :category
+  attributes :id, :name, :tags, :category, :color
   attribute :category, key: :thumb
   
   has_many :places
   
   def category
-    unless object.category.nil?
-      object.category.gsub(/\s+/, "_").downcase
+    case object.category
+    when 'Contenant'
+     'icon-glass'
+    when 'Électronique'
+     'icon-desktop'
+    when 'Lumière'
+      'icon-lightbulb'
+    when 'Maison'
+      'icon-home'
+    when 'Matériel de bureau'
+      'icon-paper-clip'
+    when 'Matériaux de construction'
+      'icon-wrench'
+    when 'Nourriture'
+     'icon-food'
+    when 'Papier'
+     'icon-file-alt'
+    when 'Santé'
+     'icon-h-sign'
+    when 'Vêtement'
+     'icon-female'
+    else
+      ''
     end
+  end
+  
+  def color
+    rand(1..8)
   end
 end
