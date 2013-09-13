0bb539cc043c62fc1d4f5f4edb04eba6ecf80cd7
Justin Domingue <justin.domingue@hotmail.com>
2013-09-12 20:45:09 -0400

    Test webhooks

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,9 +1,9 @@
 class WebhooksController < ApplicationController  
   def stripe
     data = JSON.parse(request.body.read)
-    
-    puts "Received event with ID : #{data[:id]} and Type: #{data[:type]}"
-    event = Stripe::Event.retrieve(data[:id])
-    puts event
+    puts data
+    # puts "Received event with ID : #{data[:id]} and Type: #{data[:type]}"
+    # event = Stripe::Event.retrieve(data[:id])
+    # puts event
   end
 end
