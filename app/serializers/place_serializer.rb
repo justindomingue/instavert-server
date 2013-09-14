956bfc156781999679ace69fe8cbf61dd86cfe06
Justin Domingue <justin.domingue@hotmail.com>
2013-09-14 09:14:53 -0400

    Added colors to the Place serializer

--- app/serializers/place_serializer.rb
+++ app/serializers/place_serializer.rb
@@ -1,5 +1,9 @@
 class PlaceSerializer < ActiveModel::Serializer
   embed :ids, include: true
   
-  attributes :id, :name, :tags, :thumb, :description, :responsable, :telephone, :courriel, :local, :heure, :autre
+  attributes :id, :name, :tags, :thumb, :description, :responsable, :telephone, :courriel, :local, :heure, :autre, :color
+  
+  def color
+    rand(1..8)
+  end
 end
